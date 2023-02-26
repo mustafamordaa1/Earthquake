55bdbc716aa9f4f756c134dc52c23758fff2f080
mustafamordaa1 <88613079+mustafamordaa1@users.noreply.github.com>
2023-02-26 16:50:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,4 +61,35 @@ def mainView():
     }
     return render_template('main.html', data=data)
 
+@app.route('/total/img',methods = [ 'GET'])
+def generateImg():
+	total = []
+	days = []
+	t = datetime.now()
+	today = date.today()
+	yesterday = today - timedelta(days = 1)
+	for i in range(7):
+	   URL = f"https://www.seismicportal.eu/fdsnws/event/1/query?start={yesterday}&end={today}&minlat=42.240172&maxlat=29.588440&minlon=28.533673&maxlon=49.348036"
+	   r = requests.get(URL)
+	   data = r.content
+	   data_dict = xmltodict.parse(data)
+	   json_data = json.dumps(data_dict)
+	   data = json.loads(json_data)
+	   mag = [float(i["magnitude"]["mag"]["value"]) for i in data["q:quakeml"]["eventParameters"]["event"]]
+	   total.append(len(mag))
+	   days.append(yesterday.day)
+	   today = today - timedelta(days = 1)
+	   yesterday = yesterday - timedelta(days = 1)
+	fig = Figure()
+	ax = fig.subplots()
+	ax.plot(days, total)
+	img = io.BytesIO()
+	fig.savefig(img, format="png")
+	Imgdata = base64.b64encode(img.getbuffer()).decode("ascii")
+	image = f'data:image/png;base64,{Imgdata}'
+    
+	return f"<img src='data:image/png;base64,{Imgdata}'/>"
+	
 
+if __name__ == '__main__':
+    app.run()
